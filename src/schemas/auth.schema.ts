85a397353b6eb8fa94b7cf24fb3c3a81cf5291d1
SpiffGreen <spiffjekeygreen@gmail.com>
2023-11-02 16:49:19 +0100

    refact: transform string inputs with trim

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 export const userSignupSchema = z.object({
-  fullName: z.string().min(3),
-  domainName: z.string().min(3),
-  email: z.string().email(),
+  fullName: z.string().min(3).trim(),
+  domainName: z.string().min(3).trim(),
+  email: z.string().email().trim().toLowerCase(),
   password: z.string().min(6, "Password should be at least 6 characters").regex(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
     'Weak password'
-  ), // Password must contain at least one capital letter, one small letter, one number, and one special character
+  ).trim(), // Password must contain at least one capital letter, one small letter, one number, and one special character
 });
 
 export const userSigninSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z.string().email().trim().toLowerCase(),
+  password: z.string().trim(),
 });
 
 export default {
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -2,15 +2,15 @@ import { z } from "zod";
 
 export const userUpdateSchema = z.object({
   // email: z.string().email().optional(),
-  fullName: z.string().min(3).optional(),
-  domainName: z.string().min(3).optional(),
-  username: z.string().min(3).optional(),
-  jobTitle: z.string().optional(),
-  department: z.string().optional(),
-  location: z.string().optional(),
-  language: z.string().optional(),
-  availableHoursFrom: z.string().optional(),
-  availableHoursTo: z.string().optional(),
+  fullName: z.string().min(3).trim().optional(),
+  domainName: z.string().min(3).trim().optional(),
+  username: z.string().min(3).trim().optional(),
+  jobTitle: z.string().trim().optional(),
+  department: z.string().trim().optional(),
+  location: z.string().trim().optional(),
+  language: z.string().trim().optional(),
+  availableHoursFrom: z.string().trim().optional(),
+  availableHoursTo: z.string().trim().optional(),
 });
 
 export default {
