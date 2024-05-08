a19a260becbfd8fa5341631d7dacab8bb205c6eb
Brownson <esitibrownson@gmail.com>
2024-05-08 15:16:45 +0100

    This spag won wound me!

--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { DbService } from 'src/utils/db.service';
 import { CreateStatusDTO } from './status.dto';
 
@@ -18,49 +18,105 @@ export class StatusService {
 
   async createStatus(domainID: string, dto: CreateStatusDTO) {
     try {
-      const status = await this.dbService.status.create({
-        data: {
-          name: dto.name,
-          domainId: domainID
-        }
+      const transaction: any[] = await this.dbService.$transaction([])
+
+      const existingStatuses = await this.dbService.status.findMany({
+        orderBy: { createdAt: 'asc' },
       });
+
+      const completedIndex = existingStatuses.findIndex(
+        (status) => status.name === 'completed' && status.domainId === domainID,
+      );
 
       const existingCompletedStatus = await this.dbService.status.findFirst({
         where: {
-          name: 'Completed',
-          domainId: domainID,
+          name: 'completed',
+          domainId: domainID
         }
       })
-
-      if(!existingCompletedStatus) {
-        await this.dbService.status.create({
-          data: {
-            name: 'Completed',
+  
+      if (existingCompletedStatus && completedIndex !== -1) {
+        const newStatus = this.dbService.status.create({
+          data: { 
+            name: dto.name,
             domainId: domainID
-          }
-        })
+          },
+        });
 
-        return status;
-      } else {
-        return status;
+        transaction.push(
+          this.dbService.status.update({
+            where: { id: existingStatuses[completedIndex].id },
+            data: { createdAt: new Date() },
+          }),
+        );
+        transaction.unshift(newStatus);
       }
 
+      if(!existingCompletedStatus) {
+        const newCompletedStatus = this.dbService.status.create({
+          data: {
+            name: 'completed',
+            domainId: domainID
+          },
+        });
+
+        transaction.push(newCompletedStatus);
+        
+        const newStatus = this.dbService.status.create({
+          data: {
+            name: dto.name,
+            domainId: domainID
+          },
+        });
+  
+        transaction.unshift(newStatus);
+      }
+
+  
+      await this.dbService.$transaction(transaction);
+  
+      return new HttpException('Created', HttpStatus.CREATED);
+
     } catch (error) {
+      console.log(error)
       throw new InternalServerErrorException('Status cannot be created!')
     }
   }
 
   async editStatus(statusID: string, domainId: string, dto: CreateStatusDTO) {
-    await this.dbService.status.update({ where: { id: statusID, domainId: domainId }, data: { ...dto } });
-    return {
-      message: "Updated",
+    try {
+      const existingStatus = await this.dbService.status.findUnique({
+        where: {
+          id: statusID,
+          domainId,
+        }
+      })
+
+      if(!existingStatus) throw new NotFoundException('Status not found')
+      
+      await this.dbService.status.update({ where: { id: statusID, domainId: domainId }, data: { ...dto } });
+      return new HttpException('Updated', HttpStatus.ACCEPTED)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
     }
   }
 
   async deleteStatus(statusID: string, domainId: string) {
-    await this.dbService.status.delete({ where: { id: statusID, domainId, } });
-    return {
-      message: "Deleted",
+    try {
+      const existingStatus = await this.dbService.status.findUnique({
+        where: {
+          id: statusID,
+          domainId,
+        }
+      })
+
+      if(!existingStatus) throw new NotFoundException('Status not found')
+
+      await this.dbService.status.delete({ where: { id: statusID, domainId, } });
+      return new HttpException('Deleted', HttpStatus.ACCEPTED)
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
     }
+    
   }
 }
