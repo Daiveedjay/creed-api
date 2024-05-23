664ecc2564503ab8e8336edad9396431ad007d67
SpiffGreen <spiffjekeygreen@gmail.com>
2024-05-23 17:44:43 +0100

    test: switch to ubuntu

--- deployment/user-data.sh
+++ deployment/user-data.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Update package manager and install required packages
-sudo yum update -y
-sudo yum install -y docker
+#### INSTALL DOCKER ####
+sudo apt-get update -y
+sudo apt install docker.io -y
+sudo usermod -aG docker $USER
 
-# Start the Docker service
-sudo systemctl start docker
-
-# setup caddy server
-curl -sS https://webi.sh/caddy | sh
-caddy version
+#### INSTALL CADDY SERVER
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
 
 caddy reverse-proxy --to :3000
