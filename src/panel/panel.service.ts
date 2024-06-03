115a953f9a3aa72aff8f2cc3674a616ed8bbab5b
Brownson <esitibrownson@gmail.com>
2024-06-03 18:17:05 +0100

    Invite link api changing

--- src/panel/panel.service.ts
+++ src/panel/panel.service.ts
@@ -19,6 +19,9 @@ export class PanelService {
     try {
       const panels = await this.dbService.panel.findMany({
         where: { domainId: domainID },
+        orderBy: {
+          createdAt: 'desc'
+        }
       });
       return panels;
     } catch (error) {
