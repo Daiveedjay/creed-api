bb1a87515128071106129161ff32b8a67e64124f
SpiffGreen <spiffjekeygreen@gmail.com>
2024-06-15 16:18:04 +0100

    feat: add websocket feature

--- src/collaborators/collaborators.service.ts
+++ src/collaborators/collaborators.service.ts
@@ -1,13 +1,22 @@
 /* eslint-disable prettier/prettier */
-import { HttpException, HttpStatus, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AddCollaboratorDto, JoinCollaboratorDto } from './collaborator.dto';
 import { DbService } from 'src/utils/db.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import { generateOTP } from "otp-agent";
+import { generateOTP } from 'otp-agent';
 import { InvitePayload, UserPayload } from 'src/types';
 import { UserService } from 'src/user/user.service';
 import { DomainService } from 'src/domain/domain.service';
+import { NotificationGateway } from 'src/notification/notification.gateway';
+import { NotificationEvents } from 'src/notification/notification.types';
 
 @Injectable()
 export class CollaboratorsService {
@@ -16,32 +25,36 @@ export class CollaboratorsService {
     private readonly configService: ConfigService,
     private readonly userService: UserService,
     private readonly domainService: DomainService,
+    private readonly notificationGateway: NotificationGateway,
   ) {}
 
-  async createLinkForJoining(addCollaboratorDto: AddCollaboratorDto, email: string) {
+  async createLinkForJoining(
+    addCollaboratorDto: AddCollaboratorDto,
+    email: string,
+  ) {
     try {
       const expiresAt = new Date();
-      expiresAt.setDate(expiresAt.getDate() + 1)
+      expiresAt.setDate(expiresAt.getDate() + 1);
       const otp = generateOTP({ length: 4, numbers: true, alphabets: true });
 
       const currentUser = await this.dbService.user.findUnique({
         where: {
           email,
-        }
-      })
+        },
+      });
 
-      if(!currentUser) {
-        throw new UnauthorizedException('No access')
+      if (!currentUser) {
+        throw new UnauthorizedException('No access');
       }
 
       const existingDomain = await this.dbService.domain.findUnique({
         where: {
           id: addCollaboratorDto.domainId,
-        }
-      })
+        },
+      });
 
-      if(!existingDomain) {
-        throw new NotFoundException('Domain not found!')
+      if (!existingDomain) {
+        throw new NotFoundException('Domain not found!');
       }
 
       const payload: InvitePayload = {
@@ -53,126 +66,148 @@ export class CollaboratorsService {
         expiredAt: expiresAt,
         invitedBy: {
           id: currentUser.id,
-          name: currentUser.fullName
-        }
-      }
+          name: currentUser.fullName,
+        },
+      };
 
       const hashedPayload = new JwtService().sign(payload, {
         secret: this.configService.get('JWT_SECRET'),
-      })
+      });
 
-      return `https://kreed.tech/invite?invite_code=${hashedPayload}`; 
+      return `https://kreed.tech/invite?invite_code=${hashedPayload}`;
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
   }
 
-
   async joinThroughLink(joinCollaboratorDto: JoinCollaboratorDto) {
-    const inviteeUser = await this.userService.getProfileThroughEmail(joinCollaboratorDto.email)
-  
-    const decodedPayload: InvitePayload = new JwtService().verify(joinCollaboratorDto.inviteCode, {
-      secret: this.configService.get('JWT_SECRET'),
-    });
+    const inviteeUser = await this.userService.getProfileThroughEmail(
+      joinCollaboratorDto.email,
+    );
 
-    if(!decodedPayload) {
+    const decodedPayload: InvitePayload = new JwtService().verify(
+      joinCollaboratorDto.inviteCode,
+      {
+        secret: this.configService.get('JWT_SECRET'),
+      },
+    );
+
+    if (!decodedPayload) {
       throw new UnauthorizedException('No access!');
-    };
+    }
 
     // if(decodedPayload.expiredAt < new Date()) throw new ConflictException('Link has been expired!');
 
-    const thereIsDomain = await this.domainService.getUserDomain(decodedPayload.invitedBy.id, decodedPayload.domainId)
+    const thereIsDomain = await this.domainService.getUserDomain(
+      decodedPayload.invitedBy.id,
+      decodedPayload.domainId,
+    );
 
     const alreadyInDomain = await this.dbService.domainMembership.findFirst({
       where: {
         userId: inviteeUser.id,
         domainId: thereIsDomain.id,
         memberRole: {
-          in: ['admin', 'member']
-        }
-      }
+          in: ['admin', 'member'],
+        },
+      },
     });
 
-    if(alreadyInDomain) {
-      return new HttpException('Already in domain', HttpStatus.FOUND)
-    };
+    if (alreadyInDomain) {
+      return new HttpException('Already in domain', HttpStatus.FOUND);
+    }
 
     try {
       await this.dbService.domain.update({
         where: {
-          id: thereIsDomain.id
+          id: thereIsDomain.id,
         },
         data: {
           domainMembers: {
             create: {
               userId: inviteeUser.id,
-              memberRole: decodedPayload.role
-            }
-          }
-        }
-      })
+              memberRole: decodedPayload.role,
+            },
+          },
+        },
+      });
 
-      return new HttpException(`You have successfully joined a domain: ${thereIsDomain.name}`, HttpStatus.ACCEPTED)
+      this.notificationGateway.sendAction({
+        action: NotificationEvents.JOIN_DOMAIN,
+        domain: thereIsDomain.id,
+        message:
+          '@Johnnie just joined the Company Domain, give him a warm welcome',
+      });
+
+      return new HttpException(
+        `You have successfully joined a domain: ${thereIsDomain.name}`,
+        HttpStatus.ACCEPTED,
+      );
     } catch (error) {
-      console.log(error)
-      throw new InternalServerErrorException(error.message)
+      console.log(error);
+      throw new InternalServerErrorException(error.message);
     }
   }
 
   async getAllCollaboratorsInADomain(domainId: string, email: string) {
-    const currentUser = await this.userService.getProfileThroughEmail(email)
+    const currentUser = await this.userService.getProfileThroughEmail(email);
 
-    if(!currentUser) throw new UnauthorizedException('Need to be logged in');
+    if (!currentUser) throw new UnauthorizedException('Need to be logged in');
 
-    const currentDomainAndAccess = await this.domainService.getUserDomainButLimitedAccess(domainId, currentUser.id)
+    const currentDomainAndAccess =
+      await this.domainService.getUserDomainButLimitedAccess(
+        domainId,
+        currentUser.id,
+      );
 
-    if(!currentDomainAndAccess) throw new UnauthorizedException('You do not have access!')
+    if (!currentDomainAndAccess)
+      throw new UnauthorizedException('You do not have access!');
 
-      try {
-        const domains = await this.dbService.domain.findMany({
-          where: {
-            id: currentDomainAndAccess.id,
-            OR: [
-              { ownerId: currentUser.id },  // Domains created by the user
-              { domainMembers: { some: { userId: currentUser.id } } }  // Domains joined by the user
-            ]
+    try {
+      const domains = await this.dbService.domain.findMany({
+        where: {
+          id: currentDomainAndAccess.id,
+          OR: [
+            { ownerId: currentUser.id }, // Domains created by the user
+            { domainMembers: { some: { userId: currentUser.id } } }, // Domains joined by the user
+          ],
+        },
+        include: {
+          domainMembers: {
+            select: {
+              createdAt: true,
+              id: true,
+              memberRole: true,
+              user: {
+                select: {
+                  id: true,
+                  email: true,
+                  fullName: true,
+                  username: true,
+                  jobTitle: true,
+                  department: true,
+                  location: true,
+                  profilePicture: true,
+                },
+              },
+            },
           },
-          include: {
-            domainMembers: {
-              select: {
-                createdAt: true,
-                id: true,
-                memberRole: true,
-                user: {
-                  select: {
-                    id: true,
-                    email: true,
-                    fullName: true,
-                    username: true,
-                    jobTitle: true,
-                    department: true,
-                    location: true,
-                    profilePicture: true,
-                  }
-                }
-              }
-            }
-          }
-        });
-      
-        // Extract unique members
-        const membersMap = {};
-        for (const domain of domains) {
-          for (const member of domain.domainMembers) {
-            membersMap[member.user.id] = member.user;
-          }
-        }
-        const uniqueMembers: UserPayload[] = Object.values(membersMap);
+        },
+      });
 
-        return uniqueMembers;
-      } catch (error) {
-        console.log(error)
-        throw new InternalServerErrorException('Cannot fetch the collaborators')
+      // Extract unique members
+      const membersMap = {};
+      for (const domain of domains) {
+        for (const member of domain.domainMembers) {
+          membersMap[member.user.id] = member.user;
+        }
       }
+      const uniqueMembers: UserPayload[] = Object.values(membersMap);
+
+      return uniqueMembers;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Cannot fetch the collaborators');
+    }
   }
 }
---
+++ src/notification/notification.types.ts
@@ -0,0 +1,32 @@
+export interface INotification {
+    domain: string; // in sockets.io terms rooms
+    message: string;
+}
+
+export interface INotificationAction {
+    domain: string; // in sockets.io terms rooms
+    message: string;
+    action: NotificationEvents;
+}
+
+export enum NotificationEvents {
+    GENERAL = "GENERAL",
+
+    // Room - being the domain ID, for the idea of changing domain on ui i.e joining/leaving a socket room
+    JOIN_ROOM = "JOIN_ROOM",
+    LEAVE_ROOM = "LEAVE_ROOM",
+
+    // Domain - being the domain ID, for the idea of joining/leaving the domain as a member
+    JOIN_DOMAIN = "JOIN_DOMAIN",
+    LEAVE_DOMAIN = "LEAVE_DOMAIN",
+
+    // Panel
+    CREATE_PANEL = "CREATE_PANEL",
+    EDIT_PANEL = "EDIT_PANEL",
+    DELETE_PANEL = "DELETE_PANEL",
+
+    // Task
+    CREATE_TASK = "CREATE_TASK",
+    EDIT_TASK = "EDIT_TASK",
+    DELETE_TASK = "DELETE_TASK",
+}
