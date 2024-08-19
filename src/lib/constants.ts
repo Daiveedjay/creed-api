d22f8767a1aa43ff27e37f35e379c8d7fef6e491
brownei <esitibrownson@gmail.com>
2024-08-19 12:32:10 +0100

    for redis connection

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@ const CONSTANTS = {
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
   GOOGLE_API_BASE_URL: process.env.GOOGLE_API_BASE_URL as string,
   CLIENT_APP_URL: process.env.CLIENT_APP_URL as string,
-
+  REDIS_URL: (process.env.REDIS_URL as string) || 'redis://localhost:6379',
   OTP_LENGTH: 6,
   OTP_TTL: 1, // in minutes
   RATE_LIMIT_FEEDBACK: 5, // in minutes
