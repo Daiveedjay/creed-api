b0d20fa22519469a8e35be8932a452cfba8aefdd
brownei <esitibrownson@gmail.com>
2024-09-17 12:07:14 +0100

    some bug

--- src/panel/panel.module.ts
+++ src/panel/panel.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PanelController } from './panel.controller';
 import { PanelService } from './panel.service';
+import { BullModule } from '@nestjs/bull';
 @Module({
+
+  imports: [
+    BullModule.registerQueueAsync({
+      name: 'emailQueue'
+    })
+  ],
   controllers: [PanelController],
   providers: [PanelService]
 })
-export class PanelModule {}
+export class PanelModule { }
